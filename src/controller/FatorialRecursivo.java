be8acd3b92e60818a3babd3b0a2f1a6529fe02c6
Vinicius Tiburcio <vinicius.tiburcio001@gmail.com>
2024-02-26 17:17:25 -0300

    Comment update

--- src/controller/FatorialRecursivo.java
+++ src/controller/FatorialRecursivo.java
@@ -7,10 +7,11 @@ public class FatorialRecursivo {
     }
 
     public double calcFatorial(double n){
-        if (n == 0 && n <= 12){
+        if (n == 0 && n <= 12){ //Condição de parada
             return 1;
         }else{
-            return n * calcFatorial(n - 1);
+            //Chamada função
+            return n * calcFatorial(n - 1); //Termo n em função do termo anterior: Retorna n multiplicado pelo fatorial do número anterior (n-1)
         }
     }
 }
